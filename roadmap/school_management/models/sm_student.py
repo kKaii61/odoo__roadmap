61a55cafe740595e0832ab7be0f14319d2b348a5
kainowf61 <kn06102003@gmail.com>
2025-02-11 11:31:25 +0700

    Update: Add Student model

---
+++ roadmap/school_management/models/sm_student.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+class StudentManagement(models.Model):
+    _name = 'sm.student'
+    _description = 'Student Management'
+
+    name = fields.Char('Student Name')
+    age = fields.Integer("Student Age")
+    classroom = fields.Char("Class Register")
