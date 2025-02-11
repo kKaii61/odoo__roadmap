a1934094f57f14406207ef91d293d84cbfa2a800
kainowf61 <kn06102003@gmail.com>
2025-02-11 12:42:26 +0700

    Changes

--- roadmap/school_management/models/sm_class.py
+++ roadmap/school_management/models/sm_class.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models
+from odoo import fields, models
 
 class ClassManagement(models.Model):
     _name = "sm.class"
     _description = "Class Management"
-    
+
     name = fields.Char("Class Name")
     class_type = fields.Char("Class Type")
