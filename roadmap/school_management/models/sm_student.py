90ec0c14179c10f5a9768af16f922ffc21d90b98
kainowf61 <kn06102003@gmail.com>
2025-02-12 09:01:49 +0700

    Update: Day4 complete

--- roadmap/school_management/models/sm_student.py
+++ roadmap/school_management/models/sm_student.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import api, fields, models
 
 class StudentManagement(models.Model):
     _name = 'sm.student'
@@ -9,3 +9,11 @@ class StudentManagement(models.Model):
     age = fields.Integer("Student Age", required=True)
     class_ids = fields.Many2many('sm.class','sm_class_student_relation','student_id','class_id',
                                  string="Class", ondelete="cascade", required=False)
+    # Compute to get school name
+    school_name = fields.Char(string="School Name", compute="_compute_school_name", store=True)
+    
+    @api.depends('class_ids.school_id')
+    def _compute_school_name(self):
+        for student in self:
+            schools = student.class_ids.mapped('school_id.name')  # Lấy danh sách tên trường từ class_ids
+            student.school_name = ', '.join(schools) if schools else "No School"
