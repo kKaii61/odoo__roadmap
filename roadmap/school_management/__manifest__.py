05789c38866e07be5e3c15ec47b446c9e61e7f88
kainowf61 <kn06102003@gmail.com>
2025-02-17 21:51:54 +0700

    day8: completed

--- roadmap/school_management/__manifest__.py
+++ roadmap/school_management/__manifest__.py
@@ -1,36 +1,35 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'School Management',
-    'summary': 'School Management Demo',
-    'description': """
+    "name": "School Management",
+    "summary": "School Management Demo",
+    "description": """
     A Demo App For School Management
     """,
-    'author': "Kai",
-    'version': '1.0',
-    'category': 'Basic',
-    'depends': [
-        'base','web'
+    "author": "Kai",
+    "version": "1.0",
+    "category": "Basic",
+    "depends": ["base", "web", "mail", "contacts"],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "data/cron.xml",
+        "data/sm.school.csv",
+        "data/sm.class.csv",
+        "data/mail_template_data.xml",
+        "views/sm_school_views.xml",
+        "views/sm_class_views.xml",
+        "views/sm_student_views.xml",
+        "views/menus.xml",
+        "reports/report_student.xml",
+        "reports/report_student_templates.xml",
     ],
-    'data': [
-        'security/security.xml',
-        'security/ir.model.access.csv',
-        'data/sm.school.csv',
-        'data/sm.class.csv',
-        'data/mail_template_data.xml',
-        'views/sm_school_views.xml',
-        'views/sm_class_views.xml',
-        'views/sm_student_views.xml',
-        'views/menus.xml',
-        'reports/report_student.xml',
-        'reports/report_student_templates.xml',
-    ],
-    'assets':{
-        'web.assets_backend':[
+    "assets": {
+        "web.assets_backend": [
             # 'school_management/static/src/**/*',
         ]
     },
-    'images': ['static/description/icon.png'],
-    'installable': True,
-    'application': True,
-    'license': 'LGPL-3',
+    "images": ["static/description/icon.png"],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
 }
--- roadmap/school_management/models/sm_student.py
+++ roadmap/school_management/models/sm_student.py
@@ -1,19 +1,58 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
 
-class StudentManagement(models.Model):
-    _name = 'sm.student'
-    _description = 'Student Management'
+# utils
+from datetime import date, timedelta
 
-    name = fields.Char('Student\'s Name', required=True)
+
+class StudentManagement(models.Model):
+    _name = "sm.student"
+    _description = "Student Management"
+
+    name = fields.Char("Student's Name", required=True)
     age = fields.Integer("Student's Age", required=True)
     email = fields.Char("Student's Email")
-    class_ids = fields.Many2many('sm.class','sm_class_student_relation','student_id','class_id',
-                                 string="Class", ondelete="cascade", required=False)
+
+    join_date = fields.Date(
+        "Join Date",
+        default=fields.Date.context_today,  # Current date
+        required=True,
+    )
+    graduated_date = fields.Date(
+        "Graduated Date",
+        # compute="_compute_graduated_date",  # join_date + 4years  // Comment this line to test cronjob
+        store=True,
+    )
+    status = fields.Selection(
+        [
+            ("studying", "Currently Studying"),
+            ("graduated", "Graduated"),
+            ("dropped", "Dropped Out"),
+            ("suspended", "Suspended"),
+        ],
+        string="Status",
+        compute="_compute_status",
+        store=True,
+        default="studying",  # "Currently Studying"
+    )
+
+    class_ids = fields.Many2many(
+        "sm.class",
+        "sm_class_student_relation",
+        "student_id",
+        "class_id",
+        string="Class",
+        ondelete="cascade",
+        required=False,
+    )
     # Compute to get school name
-    school_name = fields.Char(string="School Name", compute="_compute_school_name", store=True)
+    school_name = fields.Char(
+        string="School Name", compute="_compute_school_name", store=True
+    )
     # Compute to get class list
-    class_list = fields.Char(string="Class list", compute="_compute_class_list", store=True)
+    class_list = fields.Char(
+        string="Class list", compute="_compute_class_list", store=True
+    )
 
     # ====================== MODEL =========================
     #
@@ -23,10 +62,11 @@ class StudentManagement(models.Model):
         # Gọi phương thức gốc để tạo sinh viên
         student = super(StudentManagement, self).create(vals)
 
-        # Gửi email sau khi tạo
+        # Send Email After Create
         if student.email:
             student.auto_send_email()
         return student
+
     #
     #
     # ============================================================================
@@ -35,19 +75,44 @@ class StudentManagement(models.Model):
     #
     #
     def btn_email_send(self):
-        print(f"=========\nsend to {self.email} by button\n===========\n" if self.email else "\n===========\nNo email provided\n===============\n")
-        mail_template = self.env.ref('school_management.student_confirm_email', raise_if_not_found=False)
+        print(
+            f"=========\nsend to {self.email} by button\n===========\n"
+            if self.email
+            else "\n===========\nNo email provided\n===============\n"
+        )
+        mail_template = self.env.ref(
+            "school_management.student_confirm_email", raise_if_not_found=False
+        )
         if mail_template:
-            mail_template.sudo().send_mail(self.id, force_send=True)
+            try:
+                mail_template.sudo().send_mail(self.id, force_send=True)
+            except Exception as e:
+                raise e
         else:
             raise UserError("Template email not found!")
 
     def auto_send_email(self):
-        mail_template = self.env.ref('school_management.student_confirm_email', raise_if_not_found=False)
+        mail_template = self.env.ref(
+            "school_management.student_confirm_email", raise_if_not_found=False
+        )
         if mail_template:
-            mail_template.sudo().send_mail(self.id, force_send=True)
+            try:
+                mail_template.sudo().send_mail(self.id, force_send=True)
+            except Exception as e:
+                raise e
         else:
             raise UserError("Template email not found!")
+
+    def update_student_status(self):
+        """Scheduled cron job to update student statuses based on graduation date"""
+        today = date.today()
+        print("\n=====================\nCalled Update Student\n===================\n")
+        students = self.search(
+            [("graduated_date", "<=", today), ("status", "=", "studying")]
+        )
+        for student in students:
+            student.status = "graduated"
+
     #
     #
     # ============================================================================
@@ -55,15 +120,43 @@ class StudentManagement(models.Model):
     # ====================== API =========================
     #
     #
-    @api.depends('class_ids.school_id', 'class_ids')
+    @api.depends("class_ids.school_id", "class_ids")
     def _compute_school_name(self):
         for student in self:
-            schools = student.class_ids.mapped('school_id.name')  # Get school name from class_ids
-            student.school_name = ', '.join(schools) if schools else "No school engaged"
+            schools = student.class_ids.mapped(
+                "school_id.name"
+            )  # Get school name from class_ids
+            student.school_name = ", ".join(schools) if schools else "No school engaged"
+
     def _compute_class_list(self):
         for record in self:
             classes = record.class_ids
-            record.class_list = classes.mapped('name') if classes else "No class registered"
+            record.class_list = (
+                classes.mapped("name") if classes else "No class registered"
+            )
+
+    # Compute graduated date
+    @api.depends("join_date")
+    def _compute_graduated_date(self):
+        for student in self:
+            if student.join_date:
+                student.graduated_date = student.join_date + timedelta(days=4 * 365)
+            else:
+                student.graduated_date = None
+
+    # Compute status
+    @api.depends("graduated_date")
+    def _compute_status(self):
+        today = date.today()
+        for student in self:
+            if student.graduated_date and student.graduated_date <= today:
+                student.status = "graduated"
+            elif student.status not in [
+                "dropped",
+                "suspended",
+            ]:  # Not change if 'dropped' or 'suspended'
+                student.status = "studying"
+
     #
     #
     # ============================================================================
