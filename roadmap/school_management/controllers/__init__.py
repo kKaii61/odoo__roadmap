501d2e06f215ffbdec367d12938cb083ba9774ef
kainowf61 <kn06102003@gmail.com>
2025-02-17 08:26:25 +0700

    Update: day7

--- roadmap/school_management/controllers/__init__.py
+++ roadmap/school_management/controllers/__init__.py
@@ -1,1 +1,1 @@
-from . import controller
+from . import student_controller
--- roadmap/school_management/controllers/student_controller.py
+++ roadmap/school_management/controllers/student_controller.py
@@ -2,9 +2,9 @@ import json
 from odoo import http
 from odoo.http import request
 
-class getStudentList(http.Controller):
-    @http.route("/api/students", methods=["GET"], type='http', auth='none', csrf=False)
-    def getJsonData(self):
+class StudentAPI(http.Controller):
+    @http.route("/api/students", methods=["GET"], type='json', auth='none', csrf=False)
+    def get_all_student(self):
         students = request.env['sm.student'].sudo().search([])
         students_data = [
             {
@@ -20,16 +20,14 @@ class getStudentList(http.Controller):
             json.dumps(students_data),
             headers={'Content-Type': 'application/json'}
         )
-        
-class createStudent(http.Controller):
     @http.route('/api/student',methods=["POST"], type='http', auth='none', csrf=False)
-    def createStudent(self):
+    def create_student(self):
         args = request.httprequest.data.decode()
         vals = json.loads(args)
         res = request.env['sm.student'].sudo().create({
             "name": vals["name"],
             "age": vals["age"],
-            "class_ids": [(6, 0, vals["class_ids"])]
+            "class_ids": [(4, vals["class_ids"])]
         })
         # res = request.env['sm.student'].sudo().create(vals)
         if res:
